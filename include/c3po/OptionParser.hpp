a4b916128d97bf1f0df39099a37786a157a422da
zeviberlin <zeviberlin@gmail.com>
2026-04-25 16:28:15 -0400

    move into directory to ensure include uniformity

---
+++ include/c3po/OptionParser.hpp
@@ -0,0 +1,26 @@
+// The Phi Project
+// Copyright 2026 C-3PO Authors
+// Use of this source code is governed by the GPL-3.0 license, found in the LICENSE file
+//
+// Date created: 25-04-2026
+// Original author: Zevi Berlin (zeviberlin@gmail.com)
+// Latest editor: Zevi Berlin (zeviberlin@gmail.com)
+
+#ifndef C3PO_OPTION_PARSER_HPP
+#define C3PO_OPTION_PARSER_HPP
+
+#include <string>
+#include <exception>
+#include <typeinfo>
+#include <type_traits>
+
+#include "c3po/Option.hpp"
+
+//---------> [ Config. Separator ] <---------\\ 
+
+namespace C3PO {
+
+
+}
+
+#endif /* C3PO_OPTION_PARSER_HPP */
